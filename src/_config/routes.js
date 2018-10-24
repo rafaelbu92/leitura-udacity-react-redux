f5975cfdead72e0825168672737b172c24701af5
Rafael Custodio <rafael.custodio@dextra-sw.com>
2018-10-24 12:40:32 -0300

    feat: creating routes from configutarion

--- src/_config/routes.js
+++ src/_config/routes.js
@@ -1,6 +1,7 @@
 import history from './history'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { ConnectedRouter } from 'connected-react-router/lib/immutable';
 
 
 const systemRoutes = combineROutes(
@@ -12,6 +13,13 @@ const routes = combineLinkedRoutes(systemRoutes)
 
 const Routes = () => (
     <Provider store={store}>
-
+        <ConnectedRouter history={history}>
+            <Switch>
+                {systemRoutes.map((route, index) => <Route { ...route } exact key={ index }/>)}
+            </Switch>
+        </ConnectedRouter>
     </Provider>
 )
+
+export { routes }
+export default Routes
